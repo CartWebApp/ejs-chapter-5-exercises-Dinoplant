060981d1b549aa34cb6e75386016c34d1507ded0
Dinoplant <143848581+Dinoplant@users.noreply.github.com>
2024-02-15 15:48:41 -0800

    Did 2 and 3

--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,5 +1,12 @@
 // Your code here.
-
+function loop(value, test, update, body){
+    let value2 = value
+    while(test(value2)){
+    body(value2);
+    value2 = update(value2);
+    }
+      return body
+    }
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
--- 3-everything.js
+++ 3-everything.js
@@ -1,10 +1,14 @@
 function every(array, test) {
-    // Your code here.
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
   }
-  
-  console.log(every([1, 3, 5], n => n < 10));
-  // → true
-  console.log(every([2, 4, 16], n => n < 10));
-  // → false
-  console.log(every([], n => n < 10));
-  // → true
+  return test(sum)
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
